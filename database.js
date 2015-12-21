56f5f02f574e7ecd0a4629532523753ab835a918
samson8884 <samson.pinto@gmail.com>
2015-12-21 09:56:59 +0530

    added styling for login page

--- database.js
+++ database.js
@@ -1,3 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/auction');
+//mongoose.connect('mongodb://localhost:27017/auction');
+mongoose.connect('mongodb://10.44.188.88:27017/bidme-dev');
 module.exports=mongoose;
